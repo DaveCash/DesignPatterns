61746678963ddad54757ac8254421e2bbc11570c
David Cashman <david.cashman@hotmail.com>
2021-04-26 20:09:06 +0300

    Move decorator test code to separate class

--- src/DesignPatterns/ConsoleApp/Program.cs
+++ src/DesignPatterns/ConsoleApp/Program.cs
@@ -1,47 +1,15 @@
-﻿using DesignPatterns.Decorator;
-using DesignPatterns.Decorator.Decorators;
+﻿using DesignPatterns.Decorator.Commands;
 using System;
 
 namespace ConsoleApp
 {
     class Program
     {
-        private static ShapeUsageService _usageService = new ShapeUsageService();
-
         static void Main(string[] args)
         {
-            IShape square = new Square(5);
-            var decoratedSquare = AddDecorators(square);
-
-            DumpShape(square);
-            DumpShape(decoratedSquare);
-
-            var circle = new Circle(3.5M);
-            var decoratedCircle = AddDecorators(circle);
-
-            DumpShape(circle);
-            DumpShape(decoratedCircle);
-
-            Console.WriteLine($"Cumulative calculated area of decorated shapes: {_usageService.GetCumulativeCalculatedArea()}");
+            DecoratorTestCommand.Run();
 
             Console.ReadLine();
-        }
-
-        static IShape AddDecorators(IShape shape)
-        {
-            IShape decorated = new ConsoleLoggingShapeDecorator(shape);
-            decorated = new UsageRecordingShapeDecorator(decorated, _usageService);
-
-            return decorated;
-        }
-
-        static void DumpShape(IShape shape)
-        {
-            Console.WriteLine("_____________________________");
-            shape.Draw();
-            Console.WriteLine($"Area {shape.GetArea()}");
-            Console.WriteLine($"Circumference {shape.GetCircumference()}");
-            Console.WriteLine("_____________________________");
         }
     }
 }
---
+++ src/DesignPatterns/DesignPatterns/Decorator/Commands/DecoratorTestCommand.cs
@@ -0,0 +1,44 @@
+﻿using DesignPatterns.Decorator.Decorators;
+using System;
+
+namespace DesignPatterns.Decorator.Commands
+{
+    public class DecoratorTestCommand
+    {
+        private static ShapeUsageService _usageService = new ShapeUsageService();
+
+        public static void Run()
+        {
+            IShape square = new Square(5);
+            var decoratedSquare = AddDecorators(square);
+
+            DumpShape(square);
+            DumpShape(decoratedSquare);
+
+            var circle = new Circle(3.5M);
+            var decoratedCircle = AddDecorators(circle);
+
+            DumpShape(circle);
+            DumpShape(decoratedCircle);
+
+            Console.WriteLine($"Cumulative calculated area of decorated shapes: {_usageService.GetCumulativeCalculatedArea()}");
+        }
+
+        static IShape AddDecorators(IShape shape)
+        {
+            IShape decorated = new ConsoleLoggingShapeDecorator(shape);
+            decorated = new UsageRecordingShapeDecorator(decorated, _usageService);
+
+            return decorated;
+        }
+
+        static void DumpShape(IShape shape)
+        {
+            Console.WriteLine("_____________________________");
+            shape.Draw();
+            Console.WriteLine($"Area {shape.GetArea()}");
+            Console.WriteLine($"Circumference {shape.GetCircumference()}");
+            Console.WriteLine("_____________________________");
+        }
+    }
+}
